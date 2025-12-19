0eb293faf563cf8954c934c55b76ae977093ae1e
chenhao lu <luch21@mails.tsinghua.edu.cn>
2025-12-19 14:35:39 +0800

    fix: Add try-except block for DPPO algorithm imports to handle import errors

--- src/plugrl_server/algorithm/__init__.py
+++ src/plugrl_server/algorithm/__init__.py
@@ -1,5 +1,8 @@
 import plugrl_server.algorithm.dummy_algorithm
 import plugrl_server.algorithm.evaluation
 
-import plugrl_server.algorithm.dppo.dppo_config
-import plugrl_server.algorithm.dppo.dppo_dist_config
+try:
+    import plugrl_server.algorithm.dppo.dppo_config
+    import plugrl_server.algorithm.dppo.dppo_dist_config
+except Exception:
+    print(f"Could not import DPPO algorithm module for reason: {Exception}")
