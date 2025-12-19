ee71cd082fa9ecf1ad0126be4ca9873464f2241c
chenhao lu <luch21@mails.tsinghua.edu.cn>
2025-12-19 14:47:23 +0800

    fix: Improve error handling in DPPO algorithm imports by including exception details

--- src/plugrl_server/algorithm/__init__.py
+++ src/plugrl_server/algorithm/__init__.py
@@ -4,5 +4,5 @@ import plugrl_server.algorithm.evaluation
 try:
     import plugrl_server.algorithm.dppo.dppo_config
     import plugrl_server.algorithm.dppo.dppo_dist_config
-except Exception:
-    print(f"Could not import DPPO algorithm module for reason: {Exception}")
+except Exception as e:
+    print(f"Could not import DPPO algorithm module for reason: {e}")
